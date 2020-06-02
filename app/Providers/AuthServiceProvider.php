06bc3958feedcc2a00219c46dbfc82677f1f7a1e
Wilker <wilker_a@hotmail.com>
2020-06-02 15:13:59 -0300

    m22 a2

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,6 +28,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        if($this->app->runningInConsole()) return;
+
         $permissions = Permission::all();
 
         foreach ($permissions as $permission) {
