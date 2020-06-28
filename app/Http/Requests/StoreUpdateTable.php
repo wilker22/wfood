e5b18c498eaf47ebc532608f0ca02d41609ccc5f
Wilker <wilker_a@hotmail.com>
2020-06-28 17:52:06 -0300

    validações corrigidas,  com cadastro por Tenant

--- app/Http/Requests/StoreUpdateTable.php
+++ app/Http/Requests/StoreUpdateTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Tenant\Rules\UniqueTenant;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreUpdateTable extends FormRequest
@@ -26,7 +27,14 @@ class StoreUpdateTable extends FormRequest
         $id = $this->segment(3);
 
         return [
-                'identify'      => ['required','min:3','max:255',"unique:tables,identify,{$id},id"],
+                'identify'      => [
+                    //'required','min:3','max:255',"unique:tables,identify,{$id},id"
+                    'required',
+                    'min:3',
+                    'max:255',
+                    //"unique:tables,identify,{$id},id"
+                    new UniqueTenant('tables', $id),
+                ],
                 'description'   => ['nullable','min:3','max:1000'],
         ];
     }
